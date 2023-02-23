83a3d6e70c0f53501a850b82f8cc3f297f3a951c
MatevzTerziev <mt93132@student.uni-lj.si>
2023-02-23 14:05:13 +0100

    Dodaj izračun pospeška #3

--- Gravitacija.java
+++ Gravitacija.java
@@ -3,14 +3,16 @@ import java.util.Scanner;
 public class Gravitacija{
     public static void main(String[] args){
         /*Scanner sc = new Scanner(System.in);
-        double C = 6.674 * Math.pow(10,-11);
+        double v = sc.nextDouble();
+		*/
+		double C = 6.674 * Math.pow(10,-11);
         double M = 5.972 * Math.pow(10,24);
         double r = 6.371 * Math.pow(10,6);
-        double v = sc.nextDouble();
+        double v = 10;
 
         double a = (C*M)/((r+v)*(r+v));
         System.out.println(a);
-		*/
+		
 		System.out.println("OIS je zakon!");
     }
 }
